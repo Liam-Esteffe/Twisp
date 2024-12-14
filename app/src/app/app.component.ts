f7f5ac20c47040b513a825ab713e7c6a3b5101dd
Esteffe Liam <liam.esteffe2@gmail.com>
2024-12-14 12:41:58 +0100

    Update Landing page - WIP

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'app';
-
 }
